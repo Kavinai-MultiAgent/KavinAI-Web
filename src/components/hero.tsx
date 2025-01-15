5193948c4c4305cb2f26064b102bcbbdd6100f8f
unknown <apibrains@gmail.com>
2025-01-15 15:37:32 +0545

    created hero

---
+++ src/components/hero.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button } from './ui/button';
+import Image from 'next/image';
+import app_image from '../../public/app_image.jpg';
+
+export const Hero = () => {
+  return (
+    <main className='w-full h-screen bg-[#ffffff] dark:bg-[#111827]'>
+      <section className="tag_line text-center md:mx-0 mx-5">
+        <h1 className='md:text-6xl text-4xl font-bold pt-16 lg:w-[40%] md:w-[60%] w-[100%] mx-auto md:leading-[4rem] leading-[3rem]'><span className='text-purple-500'>MultiAgents</span> Made To Understand You</h1>
+        <p className='mt-2 mx-auto lg:w-[40%] md:w-[60%] w-[100%] dark:text-gray-400 text-gray-800'>An AI agent designed to control your entire PC through simple prompts, handling tasks just like a human—seamlessly, efficiently, and intuitively.</p>
+        <Button className='mt-5 md:text-xl text-lg px-7 py-7'>Download for desktop</Button>
+      </section>
+      <section className="feature_image">
+        <Image src={app_image} alt='kavinai' />
+      </section>
+    </main>
+  )
+}
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,29 +2,37 @@
 import Link from 'next/link';
 import React, { useState } from 'react';
 import { Button } from './ui/button';
-import { MenuIcon } from 'lucide-react';
+import { MenuIcon, SidebarCloseIcon } from 'lucide-react';
 
 export const Navbar = () => {
     const [nav, setNav] = useState(false);
+
+    const onCloseNavbar = () => {
+        if (nav == false){
+            setNav(true);
+        } else {
+            setNav(false);
+        }
+    }
     return (
-        <nav className='sticky top-0 left-0 z-50 flex justify-between dark:bg-black border-b border-gray-300 dark:border-gray-900 h-16 items-center px-5'>
+        <nav className='sticky top-0 left-0 z-50 flex justify-between backdrop-filter backdrop-blur-lg border-b-[.5px] border-b-blue-100 dark:border-b-[#474f57] bg-[#ffffff54] dark:bg-[#111827d8] h-16 items-center px-5'>
             <div className="left_side">
                 <div className="logo text-2xl font-bold">
                     <h1>Kavin<span className='text-blue-500'>AI</span></h1>
                 </div>
             </div>
             <div className="right_side flex items-center">
-                <ul className={`pages md:flex block md:static absolute top-16 left-0 text-center w-full justify-center md:bg-transparent dark:bg-black bg-white ${nav ? "": ""}`}>
-                    <li className='mr-3 font-semibold'><Link href='/'>Solutions</Link></li>
-                    <li className='mr-3 font-semibold'><Link href='/'>About</Link></li>
-                    <li className='mr-3 font-semibold'><Link href='/'>Pricing</Link></li>
-                    <li className='mr-3 font-semibold'><Link href='/'>FAQ</Link></li>
+                <ul className={`pages md:flex block md:static absolute top-16 left-0 text-center w-full justify-center md:bg-transparent bg-[#ffffff] dark:bg-[#111827] ${nav ? "translate-x-0": "-translate-x-full"} md:translate-x-0 duration-100`}>
+                    <li className='mr-3 font-semibold md:my-0 my-5'><Link href='/'>Solutions</Link></li>
+                    <li className='mr-3 font-semibold md:my-0 my-5'><Link href='/'>About</Link></li>
+                    <li className='mr-3 font-semibold md:my-0 my-5'><Link href='/'>Pricing</Link></li>
+                    <li className='mr-3 font-semibold md:my-0 my-5'><Link href='/'>FAQ</Link></li>
                 </ul>
                 <div className="download">
                     <Button><Link href={'/download'}>Download</Link></Button>
                 </div>
                 <div className="nav_icons md:hidden block">
-                    <MenuIcon className='cursor-pointer ml-2' />
+                    <MenuIcon onClick={onCloseNavbar} className='cursor-pointer ml-2' />
                 </div>
             </div>
         </nav>
