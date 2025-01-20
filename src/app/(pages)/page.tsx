aa564ea5dde0ae042b4a215e061f1634ec363a1e
“introlix” <apibrains@gmail.com>
2025-01-20 20:12:05 +0545

    added pricing, testimonial, use-cases, and why-us

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,17 +1,25 @@
+import { FAQ } from "@/components/faq";
 import { Hero } from "@/components/hero";
 import { PainPoints } from "@/components/pain-points";
+import { Pricing } from "@/components/pricing";
 import { SocialProof } from "@/components/social-proof";
+import { Testimonial } from "@/components/testimonial";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { UseCases } from "@/components/use-cases";
+import { WhyUs } from "@/components/why-us";
 
 
 export default function Home() {
   return (
-    <main>
+    <main className="dark:bg-gray-900">
       <Hero />
       <SocialProof/>
       <UseCases />
       <PainPoints />
+      <WhyUs />
+      <Pricing />
+      <Testimonial />
+      <FAQ />
       <ModeToggle />
     </main>
   );
